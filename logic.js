2fc4272388fbc0aa18b9e084ca9fca9937e037a4
Krzysztof Woliński <krzysztofw@kahoot.com>
2021-12-03 09:22:59 +0100

    Advent of Code 2021.3.1

--- logic.js
+++ logic.js
@@ -1,29 +1,20 @@
 module.exports = (data) => {
-  console.log(data);
-
-  const commands = data
-    .map((entry) => [entry.split(" ")[0], parseInt(entry.split(" ")[1], 10)])
-    .map(([command, value]) => ({
-      distance: command === "forward" ? value : 0,
-      aim: command === "down" ? value : command === "up" ? -value : 0,
-    }));
-
-  const { distance, depth } = commands.reduce(
-    (state, { distance, aim }) => {
-      console.log(state);
-      return {
-        ...state,
-        distance: state.distance + distance,
-        depth: state.depth + state.aim * distance,
-        aim: state.aim + aim,
-      };
-    },
-    {
-      distance: 0,
-      aim: 0,
-      depth: 0,
-    }
+  const input = data.map((record) =>
+    record.split("").map((digit) => parseInt(digit, 2))
   );
 
-  console.log(distance * depth);
+  const ratios = input.reduce(
+    (state, value) => state.map((ratio, i) => ratio + value[i]),
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  );
+
+  const gamma = ratios
+    .map((ratio) => (ratio > input.length / 2 ? 1 : 0))
+    .join("");
+
+  const epsilon = ratios
+    .map((ratio) => (ratio < input.length / 2 ? 1 : 0))
+    .join("");
+
+  console.log(parseInt(gamma, 2) * parseInt(epsilon, 2));
 };
