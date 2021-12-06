5763afbb5e5446649bbccc18fec376b18b18d7fc
Krzysztof Woliński <krzysztofw@kahoot.com>
2021-12-06 08:04:24 +0100

    Advent of Code 2021.4.1

--- logic.js
+++ logic.js
@@ -1,62 +1,103 @@
+const convertToInt = (n) => parseInt(n, 10);
+
+const sliceArray = (arr, separator) => {
+  const results = [];
+
+  arr.forEach((row) => {
+    if (row === separator || !results.length) {
+      results.push([]);
+    }
+
+    if (row !== separator) {
+      results[results.length - 1].push(row);
+    }
+  });
+
+  return results;
+};
+
+const isWinningBoard = (board) => {
+  const noOfColumns = board[0].length;
+
+  // Columns
+  for (let i = 0; i < noOfColumns; i++) {
+    if (board.every((row) => row[i].selected)) {
+      return true;
+    }
+  }
+
+  // Rows
+  if (board.some((row) => row.every((element) => element.selected))) {
+    return true;
+  }
+
+  return false;
+};
+
 module.exports = (data) => {
-  const inputs = data.map((record) =>
-    record.split("").map((digit) => parseInt(digit, 2))
+  console.log("Preparing the data...");
+
+  const [answersRaw, ...boardsRaw] = data;
+
+  const answers = answersRaw.split(",").map(convertToInt);
+  let boards = sliceArray(boardsRaw, "").map((board) =>
+    board.map((row) =>
+      row
+        .split(" ")
+        .filter((element) => element !== "")
+        .map(convertToInt)
+        .map((value) => ({
+          value,
+          selected: false,
+        }))
+    )
   );
-  const inputSize = inputs[0].length;
 
-  // Most common
-  let filteredInputs = inputs;
-  for (let position = 0; position < inputSize; position++) {
-    console.log(filteredInputs);
+  console.log("Finding the winner...");
 
-    const mostCommon =
-      filteredInputs.reduce(
-        (sum, input) => sum + parseInt(input[position], 2),
-        0
-      ) >=
-      filteredInputs.length / 2
-        ? 1
-        : 0;
+  for (let answer of answers) {
+    console.log("Answer: ", answer);
 
-    console.log("Most common: ", mostCommon);
-
-    filteredInputs = filteredInputs.filter(
-      (input) => input[position] === mostCommon
+    boards = boards.map((board) =>
+      board.map((row) =>
+        row.map((element) =>
+          element.value === answer
+            ? {
+                ...element,
+                selected: true,
+              }
+            : element
+        )
+      )
     );
 
-    if (filteredInputs.length <= 1) {
+    const winningBoards = boards.filter(isWinningBoard);
+
+    if (winningBoards.length) {
+      console.log("Winner!\n");
+
+      console.log(
+        winningBoards[0].map((row) =>
+          row.map((element) =>
+            element.selected ? `*${element.value}*` : ` ${element.value} `
+          )
+        )
+      );
+
+      const score = winningBoards[0].reduce(
+        (sum, row) =>
+          sum +
+          row.reduce(
+            (rowSum, element) =>
+              rowSum + (!element.selected ? element.value : 0),
+            0
+          ),
+        0
+      );
+
+      console.log("Score: ", score * answer);
+
       break;
     }
   }
-
-  const oxygenGeneratorRating = parseInt(filteredInputs[0].join(""), 2);
-
-  // Least common
-  filteredInputs = inputs;
-  for (let position = 0; position < inputSize; position++) {
-    console.log(filteredInputs);
-
-    const leastCommon =
-      filteredInputs.reduce(
-        (sum, input) => sum + parseInt(input[position], 2),
-        0
-      ) <
-      filteredInputs.length / 2
-        ? 1
-        : 0;
-
-    console.log("Least common: ", leastCommon);
-
-    filteredInputs = filteredInputs.filter(
-      (input) => input[position] === leastCommon
-    );
-
-    if (filteredInputs.length <= 1) {
-      break;
-    }
-  }
-
-  const co2scrubberRating = parseInt(filteredInputs[0].join(""), 2);
-
-  console.log(oxygenGeneratorRating * co2scrubberRating);
 };
